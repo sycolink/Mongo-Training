a1d69849e309664ae45dd7fd133941c2915a6efa
Lunch <vassiliou.thomas@gmail.com>
2017-08-19 16:02:25 +0100

    Rejigged project names and structure.
    C# topshelf service with log4net configured.

---
+++ MongoTrainingService/Program.cs
@@ -0,0 +1,29 @@
+﻿using Topshelf;
+using log4net.Config;
+
+namespace MongoTraining
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>                                 //1
+            {
+                XmlConfigurator.Configure();
+                x.UseLog4Net();
+                x.Service<ServiceHost>(s =>                        //2
+                {
+                    s.ConstructUsing(name => new ServiceHost());     //3
+                    s.WhenStarted(tc => tc.Start());              //4
+                    s.WhenStopped(tc => tc.Stop());  
+       //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("A simple service to play around with MongoDB");        //7
+                x.SetDisplayName("Mongo Trainings");                       //8
+                x.SetServiceName("Mongo.Training");                       //9
+            });                                                  //10
+        }
+    }
+}
---
+++ MongoTrainingService/ServiceHost.cs
@@ -0,0 +1,31 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using log4net;
+using System.Reflection;
+
+namespace MongoTraining
+{
+    class ServiceHost
+    {
+        private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        public ServiceHost()
+        {
+          
+        }
+
+        public void Start()
+        {
+            _log.Info("The service has started");
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
