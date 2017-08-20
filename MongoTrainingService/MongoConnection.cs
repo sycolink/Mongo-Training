947b2db357bd9899dc08a22a21de07ee56b8a69c
Lunch <vassiliou.thomas@gmail.com>
2017-08-20 22:05:31 +0100

    MongoConnection set up and Settings file added.

---
+++ MongoTrainingService/MongoConnection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using log4net;
+using System.Reflection;
+using MongoDB.Bson;
+
+namespace MongoTrainingService
+{
+    class MongoConnection
+    {
+        private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private IMongoClient client = new MongoClient();
+        private IMongoDatabase database;
+        private IMongoCollection<BsonDocument> collection;
+      
+
+        public MongoConnection()
+        {
+            try
+            {
+                IMongoClient client = new MongoClient(Settings.Default.MongoConnectionString);
+                database = client.GetDatabase(Settings.Default.MongoDatabase);
+                collection = database.GetCollection<BsonDocument>(Settings.Default.MongoCollection);
+
+                _log.InfoFormat("Succesfully connected to Mongo at {0}", Settings.Default.MongoConnectionString);
+            }
+            catch(Exception e)
+            {
+                _log.Error("Exception caught while connection to Mongo: ", e);
+            }
+        }
+
+        public void Insert(BsonDocument doc)
+        {
+            try
+            {
+                collection.InsertOne(doc);
+                _log.Info("Document inserted into Mongo");
+            }
+            catch (Exception e)
+            {
+                _log.Error("Error encountered inserting doucment into Mongo: ", e);
+            }         
+        }
+    }
+}
--- MongoTrainingService/ServiceHost.cs
+++ MongoTrainingService/ServiceHost.cs
@@ -7,20 +7,34 @@ using System.Threading.Tasks;
 using Topshelf;
 using log4net;
 using System.Reflection;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoTrainingService;
 
 namespace MongoTraining
 {
     class ServiceHost
     {
         private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private MongoConnection mongoConnection;
+
         public ServiceHost()
         {
-          
+        
         }
 
         public void Start()
         {
             _log.Info("The service has started");
+            mongoConnection = new MongoConnection();
+
+            BsonDocument doc = new BsonDocument
+            {
+                {"name","tom"}
+            };
+
+            mongoConnection.Insert(doc);
         }
 
         public void Stop()
